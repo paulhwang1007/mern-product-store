9dcd73c30be3dc7bf9bef5270363633b3196108a
Paul Hwang <hwpaul1007@gmail.com>
2025-01-04 23:04:48 -0800

    Set up MongoDB database
    Created a new database on MongoDB + connected the cluster + created function connectDB() to connect the database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.get("/products", (req, res) => {});
 
 app.listen(5000, () => {
+  connectDB();
   console.log("Server started at http://localhost:5000");
 });
