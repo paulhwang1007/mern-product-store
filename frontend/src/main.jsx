bc14dbd674bd7d704a2eaee7046712ecd02b2484
Paul Hwang <hwpaul1007@gmail.com>
2025-01-07 20:16:57 -0800

    Set up frontend framework
    Create the pages and components for the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "./components/ui/provider";
+import { Provider } from "./components/ui/provider.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
