7cee40c76c117eea7da720cd55de5e7ea4ff7b42
Paul Hwang <hwpaul1007@gmail.com>
2025-01-08 23:06:55 -0800

    Implement deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 // allows us to accept JSON data in the req.body
@@ -16,6 +19,15 @@ app.use("/api/products", productRoutes);
 // then we call product.route.js which checks which request it is
 // with the given id and request, it calls the respective function
 // within the controllers folder and executes
+
+// Check for the environment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
